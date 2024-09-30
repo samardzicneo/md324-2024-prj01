16f20175ee0cf68d6b01aa0308f8fbe8d5c8339b
Pedro <Pedrocorreiabatista@gmail.com>
2024-09-30 15:30:51 +0200

    idk, idgf

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Hash
+
 use Illuminate\Database\Seeder;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Support\Facades\Hash;

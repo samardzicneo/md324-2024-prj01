6b8f18df9df9d74bde93dd54554ffe50a0f115f8
Pedro <Pedrocorreiabatista@gmail.com>
2024-10-07 13:40:07 +0200

    It magically works

--- public/index.php
+++ public/index.php
@@ -9,11 +9,9 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
-print('Mashallah');
-
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+    ->handleRequest(Request::capture()); 
